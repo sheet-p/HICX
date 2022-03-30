9ff84a6946dc151ef98073d6dc846f3d310b2e54
sheet <sheetal.mec@gmail.com>
2022-03-30 22:04:44 +0100

    implementation: added test to check for processed

--- src/test/java/com/hicx/files/FileSystemTest.java
+++ src/test/java/com/hicx/files/FileSystemTest.java
@@ -1,10 +1,14 @@
 package com.hicx.files;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class FileSystemTest {
     @Test
-    public void checkIfProccesed() {
-        
+    public void checkIfProccesedExists() {
+        FileSystem dir = new FileSystem();
+
+        assertEquals(true, dir.root.dirs.containsKey("processed"));
     }
 }
