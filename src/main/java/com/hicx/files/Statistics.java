6c6e78604484696481523c4263667b3692ab75ff
sheet <sheetal.mec@gmail.com>
2022-03-30 22:18:40 +0100

    implementation: mos used word in the file

--- src/main/java/com/hicx/files/Statistics.java
+++ src/main/java/com/hicx/files/Statistics.java
@@ -1,5 +1,8 @@
 package com.hicx.files;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statistics {
     public int calcWords(String contents) {
         //to store the number of words
@@ -15,5 +18,28 @@ public class Statistics {
 
         numDots = contents.split(".").length - 1;
         return numDots;
+    }
+
+    public String mostUsedWord(String contents) {
+        //frequency table
+        Map<String, Integer> freq = new HashMap<>();
+
+        //split based on words
+        String[] words = contents.split("//s*");
+
+        for(String word : words)
+            freq.put(word, freq.getOrDefault(word, 0)+1);
+
+        //find the maximum word count
+        int wordCount = Integer.MIN_VALUE;
+        String maxWord = "";
+        for(Map.Entry<String,Integer> dict : freq.entrySet()) {
+            if(dict.getValue() > wordCount) {
+                wordCount = dict.getValue();
+                maxWord = dict.getKey();
+            }
+        }
+
+        return maxWord;
     }
 }
