526296eb0c07f2e78c89f8ecc2f9379fcfb5915d
sheet-p <sheetal.mec@gmail.com>
2022-03-31 11:17:38 +0100

    implementation: added before annotation

--- src/test/java/com/hicx/files/FileSystemTest.java
+++ src/test/java/com/hicx/files/FileSystemTest.java
@@ -3,20 +3,30 @@ package com.hicx.files;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FileSystemTest {
+    FileSystem dir;
+
+    @Before
+    public void initializations() {
+        dir = new FileSystem("/a/abc.txt");
+    }
     @Test
     public void checkIfProccesedExists() {
-        FileSystem dir = new FileSystem();
 
         assertEquals(true, dir.root.dirs.containsKey("processed"));
     }
 
     @Test
     public void checkFileMovedToProcessed() {
-        FileSystem dir = new FileSystem("/a/abc.txt");
         dir.addContentToFile("/a/abc.txt", "hicx assignment. hicx assignment is on parsing. hicx. assignment");
         assertNotNull(dir.root.dirs.get("processed"));
+    }
+
+    @Test
+    public void checkFileExistsInProcessed() {
+
     }
 }
